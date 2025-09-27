d9ddac71aada76e7b362fb101b88ce18500508e3
TimLJ <timlejemtel@gmail.com>
2025-09-27 16:26:05 -0400

    Etape 6

--- app/src/main/java/uqac/happybirthday/MainActivity.kt
+++ app/src/main/java/uqac/happybirthday/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import uqac.happybirthday.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
@@ -43,6 +44,8 @@ fun BirthdayCardPreview() {
 @Composable
 fun GreetingText(message: String, modifier: Modifier = Modifier) {
     Text(
-        text = message
+        text = message,
+        fontSize = 100.sp,
+        lineHeight = 116.sp,
     )
 }
