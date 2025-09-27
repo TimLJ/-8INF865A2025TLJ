ad5fac6c17ac6493085a504590c4fc033d53fdb0
TimLJ <timlejemtel@gmail.com>
2025-09-27 17:01:04 -0400

    Etape 7

--- app/src/main/java/uqac/happybirthday/MainActivity.kt
+++ app/src/main/java/uqac/happybirthday/MainActivity.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,7 +36,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingImage("Happy Birthday Sam!", "From Emma")
+                    GreetingImage(stringResource(R.string.happy_birthday_text),
+                        stringResource(R.string.signature_text)
+                    )
                 }
             }
         }
@@ -46,7 +49,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingImage(message = "Happy Birthday Sam!", from = "From Emma")
+        GreetingImage(stringResource(R.string.happy_birthday_text),
+            stringResource(R.string.signature_text))
     }
 }
 
